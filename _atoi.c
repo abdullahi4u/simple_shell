499093ba86c193fd3b975024aa7ef684426c6027
khaledelshafei155 <122899558+khaledelshafei155@users.noreply.github.com>
2023-04-26 11:29:21 +0200

    Update _atoi.c
    Update to better

--- _atoi.c
+++ _atoi.c
@@ -1,11 +1,9 @@
-
 #include "shell.h"
-
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
+ * interactive - checks if shell is in interactive mode
+ * @info: pointer to info_t struct containing shell information
  *
- * Return: 1 if interactive mode, 0 otherwise
+ * Return: 1 if in interactive mode, 0 otherwise
  */
 int interactive(info_t *info)
 {
@@ -13,25 +11,28 @@ int interactive(info_t *info)
 }
 
 /**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
+ * is_delim - checks if character is a delimiter
+ * @c: the character to check
+ * @delim: the delimiter string
+ *
  * Return: 1 if true, 0 if false
  */
 int is_delim(char c, char *delim)
 {
 	while (*delim)
+	{
 		if (*delim++ == c)
 			return (1);
+	}
 	return (0);
 }
 
 /**
- * _isalpha - checks for alphabetic character
- * @c: The character to input
+ * _isalpha - checks if character is an alphabetic character
+ * @c: the character to check
+ *
  * Return: 1 if c is alphabetic, 0 otherwise
  */
-
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
@@ -43,34 +44,9 @@ int _isalpha(int c)
 /**
  * _atoi - converts a string to an integer
  * @s: the string to be converted
+ *
  * Return: 0 if no numbers in string, converted number otherwise
  */
-
 int _atoi(char *s)
 {
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
-
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
-	{
-		if (s[i] == '-')
-			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
-	}
-
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
-
-	return (output);
-}
-
+	int i, sign = 1, flag =
